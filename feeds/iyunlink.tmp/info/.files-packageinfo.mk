1f03a8e3ba709557ae2803574a2cad6579df5182
yuzui66 <yuzuihong@live.com>
2023-09-26 10:32:26 +0800

    first commit

---
+++ feeds/iyunlink.tmp/info/.files-packageinfo.mk
@@ -0,0 +1,4 @@
+$(eval $(call PackageDir,quectel-CM,quectel-CM,))
+$(eval $(call PackageDir,quectel_Gobinet,quectel_Gobinet,))
+$(eval $(call PackageDir,quectel_QMI_WWAN,quectel_QMI_WWAN,))
+$(eval $(call PackageDir,ylx_hotplug,ylx_hotplug,))
